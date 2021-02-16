4a75d856e2e13c516c4de13cd89b9949a040c054
alexClifton <oooalexoooo@gmail.com>
2021-02-15 20:18:34 -0800

    basic swapi

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
 
+function createInitialPlanets() {
+  return ["earth", "moon"]
+}
+
+const URL = "https://swapi.dev/api/planets/1";
+
 function App() {
+  const [ planets, setPlanets ] = React.useState(createInitialPlanets())
+
+  function handleClick() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(res => {
+        setPlanets([...planets, res.name])
+      })
+      .catch(e => console.log(e))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <button onClick={handleClick}>GET MORE PLANETS</button>
+      <h1>swapi grab</h1>
+      <ul>
+        {
+          planets.map(planet => {
+            return (
+              <li>{planet}</li>)
+          })
+        }
+      </ul>
     </div>
   );
 }
