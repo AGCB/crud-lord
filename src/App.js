4f0109f1d2f3212cd3e5f2759bf962d10ba7cf1c
alexClifton <oooalexoooo@gmail.com>
2021-02-23 18:19:52 -0800

    infinite-scroll test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 
 function createInitialPlanets() {
   return ["earth", "moon"]
 }
 
-const URL = "https://swapi.dev/api/planets/1";
+const URL = "https://swapi.dev/api/planets/";
 
 function App() {
   const [ planets, setPlanets ] = React.useState(createInitialPlanets())
+  const [ count, setCount ] = useState(1)
+  const [ isMounted, setIsMounted ] = useState(false);
+  const footerRef = useRef();
+  const handleClickRef = useRef();
 
-  function handleClick() {
-    fetch(URL)
+  const handleClick = useCallback(() => {
+    console.log('!!! inside handleClick')
+    fetch(URL+String(count))
       .then(res => res.json())
       .then(res => {
         setPlanets([...planets, res.name])
+        setCount(count+1)
       })
       .catch(e => console.log(e))
-  }
+  },[planets, count]);
+
+  // keep callBackRef up to date
+  useEffect(() => {
+    // updating some current ...
+    handleClickRef.current = handleClick;
+  }, [handleClick])
+
+  useEffect(() => {
+    const myObserver = new IntersectionObserver(() => handleClickRef.current(), {threshold: .1})
+    myObserver.observe(footerRef.current)
+
+  }, [])
+
 
   return (
     <div className="App">
@@ -27,12 +46,31 @@ function App() {
         {
           planets.map(planet => {
             return (
-              <li>{planet}</li>)
+              isLoading ? <span>???</span>: <li
+                key={planet}
+                style={{
+                  "height": "50px",
+                  "border": "30px solid red"
+                }}>{planet}</li>)
           })
         }
       </ul>
+      <div
+        ref={footerRef}
+        style={{
+          "height": "50px",
+          "backgroundColor": "blue"
+        }}>placeholder for breaking</div>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+
+function potatoSlicer(str) {
+
+}
+ */
